4a2b169294e302267abd2e94bfafe67226766d03
Clinton Davelaar <davelaarprogramming@gmail.com>
2021-02-06 15:59:47 -0600

    Initial Commit

---
+++ TabbedAppSwiftUI/TabbedAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabbedAppSwiftUIApp.swift
+//  TabbedAppSwiftUI
+//
+//  Created by student on 2/6/21.
+//
+
+import SwiftUI
+
+@main
+struct TabbedAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
